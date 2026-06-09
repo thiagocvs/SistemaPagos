0f3b42c441fcba0746d55d9dadad11c481777308
Jeremias Herrera <herrera.jere06@gmail.com>
2026-06-09 20:47:40 -0300

    clase efectivo

--- SistemaPago/entities/Efectivo.cs
+++ SistemaPago/entities/Efectivo.cs
@@ -3,7 +3,7 @@ public class Efectivo : Pago
     private string Divisa;
     public Efectivo(string titular, double montoOriginal, DateTime fecha, string divisa) : base(titular, montoOriginal, fecha)
     {
-        this.Divisa1 = divisa;
+        this.Divisa = divisa;
     }
 
     public string divisa { get => Divisa; set => Divisa = value; }
